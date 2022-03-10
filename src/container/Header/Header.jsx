237b28b7278127816da808a9541ae8c92edbb142
achakarov <alexander.chakarov1@gmail.com>
2022-03-10 17:25:18 +0200

    added css and lorem ipsum text to the header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,9 +10,19 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        ultrices velit in suscipit dapibus. Suspendisse a risus posuere, egestas
+        nulla.
+      </p>
+      <button type="button" className="custom__button">
+        Explore More
+      </button>
     </div>
 
-    <div className="app_wrapper_img"></div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
